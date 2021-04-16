55632babded867532fcbbe388caebbfc7475f42a
Tom Horan <tomhoran91@gmail.com>
2021-04-16 16:25:18 +0200

    added cloundinary image

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import {Image} from 'cloudinary-react';
 
 
 function App() {
@@ -7,11 +8,22 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h1>
-          My portfolio page
+          Tom Horan - Web Devloper, Cool Guy
         </h1> 
-        <img src="https://res.cloudinary.com/tom-horan/image/upload/v1618494442/LeWagonPicTH_r8xt8v.jpg" alt="Tom Horan" class="profile-image" />
       </header>
+      <body>
+        <div class="project-list">
+          <ul>
+            <li>Project 1</li>
+            <li>Project 2</li>
+            <li>Project 3</li>
+            <li>Project 4</li>
+          </ul>
+        </div>
+        <Image cloudName="tom-horan" publicId="https://res.cloudinary.com/tom-horan/image/upload/c_crop,h_700,w_1400,x_300,y_200/v1618494442/LeWagonPicTH_r8xt8v.jpg" ></Image>
+      </body>
     </div>
+    
   );
 }
 
