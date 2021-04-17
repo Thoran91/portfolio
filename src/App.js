ef2b08044c118ef1925ec309f198a941256aa0e9
Tom Horan <tomhoran91@gmail.com>
2021-04-17 14:49:57 +0200

    Cropped header image and text

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import './App.css';
-import {Image} from 'cloudinary-react';
-
 
 function App() {
 
   return (
     <div className="App">
       <header className="App-header">
-        <h1>
-          Tom Horan - Web Devloper, Cool Guy
+        <h1 class="header-text">
+          Tom Horan
+        </h1>
+        <h1 class="header-text">
+          Front End Web Developer
         </h1> 
       </header>
       <body>
@@ -20,7 +21,6 @@ function App() {
             <li>Project 4</li>
           </ul>
         </div>
-        <Image cloudName="tom-horan" publicId="https://res.cloudinary.com/tom-horan/image/upload/c_crop,h_700,w_1400,x_300,y_200/v1618494442/LeWagonPicTH_r8xt8v.jpg" ></Image>
       </body>
     </div>
     
