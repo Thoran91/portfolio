f26dbfa7494edead886f44cda953e641e61d0d4a
Tom Horan <tomhoran91@gmail.com>
2021-05-18 15:33:06 +0200

    something

--- src/App.js
+++ src/App.js
@@ -4,13 +4,12 @@ function App() {
 
   return (
     <div className="App">
+      <script src="https://kit.fontawesome.com/7b66af3075.js" crossorigin="anonymous"></script>
       <div class="container">
         <header className="app-header">
           <link rel="preconnect" href="https://fonts.gstatic.com"></link>
           <link href="https://fonts.googleapis.com/css2?family=Inter:wght@700&display=swap" rel="stylesheet"></link>
           <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/devicons/devicon@v2.11.0/devicon.min.css"></link>
-          <script src="https://kit.fontawesome.com/7b66af3075.js" crossorigin="anonymous"></script>
-          <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css"></link>
           <h1 class="header-text">
             Tom Horan
           <br/>
@@ -82,9 +81,9 @@ function App() {
           </div>
         </body>
         <footer className="app-footer d-flex">
-          <a href="mailto:tomhoran91@gmail.com"><i class="fa fa-envelope email-red fa-2x"></i>thorandev91@gmail.com</a>
-          <a href="https://www.linkedin.com/in/tom-horan/"><i class="fa fa-linkedin linkedin-blue fa-2x"></i>LinkedIn</a>
-          <a href="https://github.com/Thoran91"><i class="fa fa-github-square github-purple fa-2x"></i>GitHub</a>
+          <a href="mailto:tomhoran91@gmail.com"><i class="fas fa-envelope email-red fa-2x"></i>thorandev91@gmail.com</a>
+          <a href="https://www.linkedin.com/in/tom-horan/"><i class="fab fa-linkedin linkedin-blue fa-2x"></i>LinkedIn</a>
+          <a href="https://github.com/Thoran91"><i class="fab fa-github-square github-purple fa-2x"></i>GitHub</a>
         </footer>
       </div>
     </div>
